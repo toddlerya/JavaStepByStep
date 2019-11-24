eb70625d2ba0abeddabc20bb39196c9415172e01
toddlerya <toddlerya@gmail.com>
2019-11-24 18:08:23 +0800

    画了个圆

---
+++ exercises/04/Circle.java
@@ -0,0 +1,82 @@
+public class Circle {
+    public static void main(String[] args) {
+        // 测试距离计算方法
+        // int[] x = new int[]{3, 0};
+        // int[] y = new int[]{0, 4};
+        // System.out.print(distance(x, y));
+        draw(13);
+    }
+
+    /**
+     * 根据不同半径打印不同大小的圆
+     * @param r 圆形的半径
+     */
+    public static void draw(int r) {
+        double precision = 0.4;
+        // 初始化圆心坐标
+        int[] centerPoint = new int[]{r, r};
+        // 第一象限坐标系，圆的外切正方形
+        int width, height;
+        width = height = 2 * r;
+        // 每一个点
+        int[] eachPoint = new int[2];
+        // 计算每个点与圆心的距离是否趋近于r, 误差为precision
+        for (int w = 0; w <= width; w++) {
+            for (int h = 0; h <= height; h++) {
+                eachPoint[0] = w;
+                eachPoint[1] = h;
+                double diffValue = distance(centerPoint, eachPoint) - (double)r;
+                if (0 <= diffValue && diffValue <= precision) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+    /**
+     * 计算两点距离
+     */
+    public static double distance(int[] aPoint, int[] bPoint) {
+        double d;
+        d = Math.sqrt(Math.pow((aPoint[0] - bPoint[0]), 2) + Math.pow((aPoint[1] - bPoint[1]), 2));
+        return d;
+    }
+}
+
+/*
+# evi1 @ evi1nullMacBook-Pro in ~/JavaStepByStep/exercises/04 on git:master x [18:06:05] C:1
+$ javac Circle.java
+(base)
+# evi1 @ evi1nullMacBook-Pro in ~/JavaStepByStep/exercises/04 on git:master x [18:06:33]
+$ java Circle
+                    * * * * * * *
+                *                   *
+            *                           *
+
+
+
+    *                                           *
+
+  *                                               *
+
+*                                                   *
+*                                                   *
+*                                                   *
+*                                                   *
+*                                                   *
+*                                                   *
+*                                                   *
+
+  *                                               *
+
+    *                                           *
+
+
+
+            *                           *
+                *                   *
+                    * * * * * * *
+
+*/
--- exercises/04/IsoscelesTriangle.java
+++ exercises/04/IsoscelesTriangle.java
@@ -1,19 +1,24 @@
 public class IsoscelesTriangle {
     public static void main(String[] args) {
         draw(4);
+        draw(30);
     }
 
+    /**
+     * 根据输入高度打印等腰三角形
+     * @param row 三角形高度（行数）
+     */
     public static void draw(int row) {
         int colNum = 2 * row - 1;
         for (int i = 1; i <= row; i++) {
-                int starNum = 2 * i - 1;
-                int blankNum = (colNum - starNum) / 2;
-                for (int b = 0; b < blankNum; b++) {
-                    System.out.print(" ");
-                }
-                for (int s = 0; s < starNum; s++) {
-                    System.out.print("*");
-                }
+            int starNum = 2 * i - 1;
+            int blankNum = (colNum - starNum) / 2;
+            for (int b = 0; b < blankNum; b++) {
+                System.out.print(" ");
+            }
+            for (int s = 0; s < starNum; s++) {
+                System.out.print("*");
+            }
             System.out.println();
         }
     }
