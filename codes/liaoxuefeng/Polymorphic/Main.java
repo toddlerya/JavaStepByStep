41f143a47602e37530c5dda0b37b1d6dae30d08f
toddlerya <toddlerya@gmail.com>
2019-12-20 00:08:39 +0800

    day day up

---
+++ codes/liaoxuefeng/Polymorphic/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(final String[] args) {
+        
+    }
+}
+    
+
+class Person {
+    public void run() {}
+}
+
+public class Student extends Person {
+    @Override
+    public void run(final String a) {}
+}
